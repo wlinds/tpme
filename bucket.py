da197f1a3391f62cd774ffdadc149cab3b5f2c14
William L <41084946+helveticae@users.noreply.github.com>
2023-02-11 17:15:18 +0100

    names mostly assembled by chatGPT

---
+++ bucket.py
@@ -0,0 +1,37 @@
+# This should be zipped/compressed in other format, as of now it works but it's messy
+
+m_sweden_gpt = ["Lars", "Anders", "Johan", "Karl", "Per", "Mikael", "Jan", "Henrik", "Björn", "Erik", "Ingvar", "Hans", "Gustaf", "Nils", "Sven", "Olof", "Tomas", "Bo", "Urban", "Stig", "Lennart", "Ruben", "Kjell", "Emil", "Oskar", "Rolf", "Fredrik", "Magnus", "Jakob", "Petter", "Andreas", "Daniel", "Pelle", "Mathias", "Jonathan", "Viktor", "Isak", "David", "Anton", "Emilio", "Joel", "Adam", "Robin", "Leif", "Max", "Filip", "Alexander", "Martin", "William", "Göran", "Carl", "Mats", "Samuel", "Peder", "Thomas", "John", "Staffan", "Markus", "Gustav", "Bengt", "Jens", "Richard", "Jim", "Mattias", "Patrik", "Jörgen", "Olivia", "Stefan", "Christoffer", "Jimmie", "Marcus", "Elias", "Maurice", "Ulf", "Christian", "Håkan", "Marcus", "Gunnar", "Bertil", "Tobias", "Ingemar", "Mikael", "Linus", "Dennis", "Alfred", "Hans", "Sebastian", "Arne", "Jesper", "Janne", "Christer"]
+f_sweden_gpt = ["Maria", "Anna", "Margareta", "Elisabeth", "Eva", "Birgitta", "Ingrid", "Karin", "Kristina", "Lena", "Marianne", "Inger", "Greta", "Annika", "Ulla", "Gunilla", "Lilian", "Astrid", "Sofia", "Agneta", "Yvonne", "Monica", "Elvira", "Ingegerd", "Bodil", "Annie", "Linn", "Ester", "Carina", "Sara", "Helena", "Emma", "Therese", "Lise-Lotte", "Kicki", "Lillemor", "Lottie", "Henny", "Lena", "Maud", "Jenny", "Lissie", "Livia", "Loulou", "Luna", "Linda", "Anita", "Lotta", "Louise", "Gunvor", "Linnea", "Inga", "Irene", "Stina", "Liv", "Mimmi", "Madeleine", "Maj-Britt", "Anette", "Mona", "Pia", "Inger", "Fanny", "Gudrun", "Barbro", "Ann-Sofie", "Charlotte", "Hanna", "Ida", "Rut", "Anna-Lena", "Ellen", "Cecilia", "Ebba", "Anita", "Linda", "Maj", "Frida", "Tova", "Viktoria", "Camilla", "Emilia", "Marta", "Nina", "Märta", "Maja", "Amanda", "Rebecca", "Julia", "Johanna"]
+f_norwa_list = ['Nora', 'Norah', 'Emma', 'Ella', 'Maja', 'Maia', 'Maya', 'Olivia', 'Emilie', 'Sofie', 'Sophie', 'Leah', 'Sofia', 'Sophia', 'Ingrid', 'Frida', 'Sara', 'Sarah', 'Zara', 'Tiril', 'Selma', 'Ada', 'Hedda', 'Amalie', 'Anna', 'Alma', 'Hanna', 'Eva', 'Mia', 'Thea', 'Live', 'Ida', 'Astrid', 'Ellinor', 'ilde', 'Linnea', 'Iben', 'Aurora', 'Mathilde', 'Mathilde', 'Saga', 'Solveig', 'Marie', 'Eline', 'Oline', 'Maria']
+
+m_scandi_gpt = ["Oliver", "Noah", "William", "Emil", "Liam", "Elias", "Oskar", "Axel", "Lucas", "Filip",
+"Erik", "Isak", "Mikael", "Jakob", "Leo", "Simon", "Theodor", "Viktor", "Sebastian", "Alexander",
+"Hampus", "Hugo", "Adrian", "Max", "David", "Fredrik", "Linus", "Edward", "Olle", "Arvid",
+"Carl", "Henrik", "Kevin", "Elvin", "Nils", "Joel", "Magnus", "Daniel", "Mathias", "Anton",
+"Linus", "Andreas", "Sondre", "Gustav", "Niklas", "Hannes", "Emilio", "Johan", "August", "Aron",
+"Vemund", "Jens", "Markus", "Björn", "Johannes", "Viggo", "Ludvig", "Rasmus", "Benjamin", "Isac",
+"Tomas", "Mats", "Eirik", "Olav", "Peder", "Kai", "Sander", "Anders", "Stian", "Even",
+"Eirik", "Sverre", "Jakob", "Johan", "Tommy", "Truls", "Espen", "Marius", "Krister", "Geir"]
+
+f_scandi_gpt = ["Emma", "Sofia", "Ella", "Maja", "Aurora", "Linn", "Nora", "Ida", "Molly", "Hanna",
+"Lena", "Alma", "Violet", "Ingrid", "Frida", "Emilia", "Leah", "Livia", "Signe", "Linnea",
+"Amanda", "Evy", "Tuva", "Thea", "Sara", "Camilla", "Julia", "Elise", "Amalie", "Eline",
+"Ada", "Marie", "Emma", "Sofie", "Johanna", "Mia", "Ester", "Isabelle", "Martine", "Inger",
+"Karoline", "Maria", "Ragnhild", "Synne", "Silje", "Annie", "Ingrid", "Tiril", "Eli", "Iben",
+"Marte", "Andrea", "Tonje", "Rikke", "Anette", "Lise", "Elina", "Ellen", "Stine", "Ingvild",
+"Helene", "Karen", "Guro", "Sandra", "Marianne", "Maren", "Turid", "Lene", "Bente", "Anne",
+"Ingrid", "Bodil", "Grete", "Rita", "Astrid", "Berit", "Kari", "Gro", "Lillian", "Unni"]
+
+m_slavic_gpt = ["Ivan", "Vladimir", "Sergei", "Alexander", "Dmitri", "Maxim", "Nikolai", "Andrei", "Viktor", "Pavel",
+"Yuri", "Mikhail", "Boris", "Stanislav", "Oleg", "Evgeny", "Anton", "Konstantin", "Aleksandr", "Semyon"]
+
+f_slavic_gpt = ["Anna", "Olga", "Maria", "Elena", "Natalia", "Tatiana", "Svetlana", "Irina", "Nadezhda", "Larisa",
+"Yelena", "Lyubov", "Alla", "Veronika", "Irina", "Galina", "Ksenia", "Ekaterina", "Alina", "Anastasia"]
+
+
+last_swe = ['Svensson', 'Andersson', 'Gustavsson', 'Gjertsson', 'Nilsson', 'Knutsson', 'Fredriksson', 'Lundgren', 'Fransson', 'Lundgren', 'Lindstedt', 'Mattsson', 'Ohlsson', 'Bengtsson', 'Lundberg', 'Lindberg']
+last_gpt_eur0 =['Oliveira', 'Sandberg', 'Martin', 'Sousa', 'Garrido', 'Fonseca', 'Bjork', 'Gouveia', 'Pérez', 'Navarro', 'Silva', 'Pascual', 'Matos', 'Serrano', 'Ferreira', 'Jimenez', 'Lopez', 'Blanco', 'Romero', 'Edwards', 'Gonzalez', 'Santana', 'Coelho', 'Barros', 'Vieira', 'Larsson', 'Gomes', 'Castillo', 'Lundberg', 'Karlsson', 'Rodriguez', 'Berglund', 'Olsson', 'Cuesta', 'Diaz', 'Alonso', 'Andersson', 'Gomez', 'Bergstrom', 'Bengtsson', 'del Rio', 'Jonsson', 'Sanchez', 'Vazquez', 'Ribeiro', 'Jansson', 'Hidalgo', 'Castro', 'Petersson', 'Hansson', 'Carlsson', 'Vidal', 'Morales', 'Rivera', 'Santos', 'Faria', 'Gimenez', 'Sjogren', 'Perez', 'Andrade', 'Pacheco', 'Jakobsson', 'Berggren', 'Forsberg', 'Mendes', 'Ruiz', 'Jesus', 'Cardoso', 'Rodríguez', 'Eklund', 'Nunes', 'Ramalho', 'Persson', 'Eriksson', 'Gustafsson', 'Wallin', 'Soderberg', 'Molina', 'Garcia', 'Moreno', 'Alves', 'Rodrigues', 'Rocha', 'Barbosa', 'Alvarez', 'Martinez', 'Johansson', 'Svensson', 'Moura', 'Monteiro', 'Fernandez', 'Lopes', 'Pettersson', 'Suarez', 'Ortega', 'Costa', 'Pereira', 'Dominguez', 'Ramirez', 'Gómez', 'Teixeira', 'Ramos', 'Reyes', 'Lundqvist', 'Sundberg', 'Carvalho', 'Cabrera', 'Fernández', 'Martins', 'Torres', 'Pinto', 'Nilsson', 'Almeida', 'Lima', 'Neves', 'Hernandez', 'Vera', 'Marques']
+last_gpt_eur1 = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson"] # This doesn't seem very scandinavian
+last_gpt_eur2 = ["Schmidt", "Schneider", "Fischer", "Müller", "Weber", "Schulz", "Wagner", "Becker", "Hoffmann", "Schäfer", "Koch", "Bauer", "Richter", "Klein", "Wolf", "Schröder", "Neumann", "Schwarz", "Braun", "Hofmann"] # This is also very weirdly distributed for a 'european name' prompt
+last_gpt_asia = ["Kim", "Lee", "Park", "Wong", "Choi", "Li", "Zhang", "Lin", "Wu", "Chen", "Yuan", "Zhao", "Yang", "Huang", "Liu", "Guo", "Wang", "Sun", "Xu", "Gao"]
+last_gpt_mena = ["Al-Saud", "Al-Shaikh", "Al-Maktoum", "Al-Sabah", "Al-Thani", "Bin Laden", "Al-Nasser", "Al-Hussein", "Al-Assad", "Al-Khalifa", "Al-Mahmoud", "Al-Hajri", "Al-Dhabi", "Al-Otaibi", "Al-Rumaihi", "Al-Ghanim", "Al-Mulla", "Al-Hariri", "Al-Mutairi", "Al-Sayed", "Hussein", "Mohammed", "Ahmed", "Ali", "Mahmoud", "Omar", "Abdullah", "Youssef", "Sayed", "Ibrahim", "Khan", "Hasan", "Qassem", "Rashid", "Tariq", "Jamal", "Faris", "Nashat", "Taleb", "Ghanem"]
