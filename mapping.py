cca98bb091e5db9a124e553f06e6597ca23a496d
William L <41084946+helveticae@users.noreply.github.com>
2023-02-11 17:16:11 +0100

    Initial commit

---
+++ mapping.py
@@ -0,0 +1,41 @@
+# Assign gender
+kön_map = {1: 'Kvinna', 2: 'Man', 3: 'Annat',}
+
+# Assign civilstånd
+civil_map = {1: 'Gift/Sambo',
+           2: 'Änka/Änkling ej omgift',
+           3: 'Skild, ej omgift',
+           4: 'Särbo',
+           5: 'Ogift',
+           6: 'Annat',}
+
+# Assign utbildningsnivå
+utbil_map = {1: 'Folkskolenivå eller lägre',
+           2: 'Grundskola/Realexamen',
+           3: 'Studentexamen',
+           4: 'Fölkhögskola/Yrkesutbildning',
+           5: 'Ofullständig akademisk utbildning',
+           6: 'Universitet/Högskola med examen',}
+
+# Assign sysselsättning
+syssl_map = {1: 'Arbete', 
+           2: 'Studier', 
+           3: 'Arbetslös', 
+           4: 'Sjukskriven/sjukersättning', 
+           5: 'Ålderspensionär', 
+           6: 'Icke-förvärvsarbetande av annan orsak',}
+
+# Assign boende
+boend_map = {1: 'Hyresrätt', 
+           2: 'Bostadrätt', 
+           3: 'Radhus, villa eller liknande', 
+           4: 'Särskilt boende', 
+           5: 'Annat',}
+
+# Assign living situation
+bormd_map = {1: 'Ensamboende', 
+           2: 'Make/maka/sambo', 
+           3: 'Barn', 
+           4: 'Förälder', 
+           5: 'Annan',
+           6: 'Familj (Partner och barn/ föräldrar och syskon),'}
