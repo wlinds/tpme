6239b8bbae8f19f17970d5d4f2dfc396f5e57bd4
William L <41084946+helveticae@users.noreply.github.com>
2024-02-11 19:28:02 +0100

    add subplot fn

--- utils/plots.py
+++ utils/plots.py
@@ -9,3 +9,24 @@ def get_histogram(df, title, xlabel, ylabel, bins):
     plt.ylabel(ylabel)
     plt.grid(True)
     plt.show()
+
+
+def get_histogram_subplots(df, title, xlabel, ylabel, bins):
+    plt.figure(figsize=(15, 6))
+
+    plt.subplot(1, 2, 1)
+    sns.histplot(df[0], bins=bins[0], kde=True, color='skyblue', edgecolor='black')
+    plt.title(title[0])
+    plt.xlabel(xlabel[0])
+    plt.ylabel(ylabel[0])
+    plt.grid(True)
+
+    plt.subplot(1, 2, 2)
+    sns.histplot(df[1], bins=bins[1], kde=True, color='skyblue', edgecolor='black')
+    plt.title(title[1])
+    plt.xlabel(xlabel[1])
+    plt.ylabel(ylabel[1])
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
