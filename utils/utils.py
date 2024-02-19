7ed5ab62ef7c6a9b00707e1cfea59da545fd3a6f
William L <41084946+helveticae@users.noreply.github.com>
2024-02-19 20:20:40 +0100

    move expenses to own script

--- utils/utils.py
+++ utils/utils.py
@@ -67,53 +67,6 @@ def generate_datetime(date_threshold=None, time_threshold=None, deviation_p=0.01
     return res
 
 
-def get_expenses():
-    market = pd.read_csv("Data/datasets/tamimimarkets.csv")
-    general = pd.read_csv("Data/datasets/Ronash_DS_Assignment.csv")
-
-    market['Category'] = 'Food, Beverages & Tobacco'
-    market['tags'] = "['snacks']"
-    market = market.drop('Size', axis=1)
-    market = market.rename(columns={'Prices': 'Price', 'Company': 'Vendor', 'Unnamed: 0': 'product_id'})
-
-    general['Price'] = get_price()
-    general['Currency'] = 'SEK'
-    general = general.rename(columns={'title': 'Product'})
-    general = general.rename(columns={'vendor': 'Vendor', 'category': 'Category'})
-    general = general[['product_id', 'Vendor', 'Product', 'Currency', 'Price', 'Category', 'tags']]
-
-    merged_df = pd.concat([general, market], ignore_index=True)
-
-    return merged_df
-
-
-def get_price():
-    # Placeholder # TODO
-    return 200.00
-
-
-def generate_expenses(keyword=None, category=None):
-    df = get_expenses()
-
-    if category:
-        df = df[df['Category'] == category]
-
-    if keyword:
-        df = df[df['tags'].str.contains(keyword, case=False, na=False)]
-
-    selected_row = df.sample(1, random_state=None)
-
-    expense_details = {
-        'Category': selected_row['Category'].values[0],
-        'Product': selected_row['Product'].values[0],
-        'Price': selected_row['Price'].values[0],
-        'Currency': selected_row['Currency'].values[0],
-        'Datetime': generate_datetime(deviation_p=0)
-    }
-
-    return expense_details
-
-
 if __name__ == "__main__":
     print(generate_datetime())
     print(generate_datetime(('2012-01-01','2012-12-12'),('08:00:01','09:01:13')))
