615058f0f6980d0d2792f331da06c8dea261a1fa
William L <41084946+helveticae@users.noreply.github.com>
2024-03-21 22:00:30 +0100

    improve anonymize phone number fn

--- main.py
+++ main.py
@@ -212,24 +212,24 @@ def gen_health(mean=3, std=1, skewness=0):
 
 
 def gen_phone(anonymize=False):
-  # challenge# 1: needs to be unique
-  # for now it will have a probability of 2.474631929433396e-07 to be duplicare
+    # challenge# 1: needs to be unique
+    # for now it will have a probability of 2.474631929433396e-07 to be duplicare
 
-  # Swedish phone# TODO add other 
-  a = str(np.random.choice([70, 72, 73, 76, 79]))
-  b = np.random.randint(100, high=999)
-  c = np.random.randint(100, high=999)
+    # Swedish phone# TODO add other 
+    a = str(np.random.choice([70, 72, 73, 76, 79]))
+    b = np.random.randint(100, high=999)
+    c = np.random.randint(100, high=999)
 
-  if anonymize:
-    a = 'XX'
+    if anonymize:
+        a = 'XX'
 
-  def add_zero(n):
-      if n < 10: n = '00' + str(n)
-      elif n < 100: n = '0' + str(n)
-      return str(n)
+    def add_zero(n):
+        if n < 10: n = '00' + str(n)
+        elif n < 100: n = '0' + str(n)
+        return str(n)
 
-  phone = str(0) + a + str(np.random.randint(0,9)) + add_zero(b) + add_zero(c)
-  return phone
+    phone = str(0) + a + str(np.random.randint(0,9)) + add_zero(b) + add_zero(c)
+    return phone
 
 
 def gen_customerID():
@@ -274,7 +274,7 @@ class PersonGenerator:
 
         # Just ints
         age = gen_age(**dist_age)
-        phone = gen_phone()
+        phone = gen_phone(anonymize=self.anonymize)
         gendr = gen_gender(**dist_gender)
         civil = gen_marital(age)
         utbil = gen_education(age)
